70efb3fc3c14901ba7141f8b25a0b8bad968e8f7
Albina <albine89@mail.ru>
2022-11-28 23:13:19 +0300

    correct

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[] { "test", "12", "0", "w" };
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        
+            if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+            else Console.Write($"{arr[i]}");
+        
+    }
+    Console.WriteLine("]");
+}
+
+string[] CreateNewArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] newArr = new string[count];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    return newArr;
+}
+
+string[] newArray = CreateNewArray(array);
+PrintArray(newArray);
